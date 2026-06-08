5c9b00b961b074b7856a0a239b12d9c929764866
JannatRahman <you@example.com>
2026-06-08 20:45:06 +0600

    backend connected with frontend

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 5000
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+
+       const database = client.db("hireloop-db");
+    const jobCollection = database.collection("jobs");
+    const companyCollection = database.collection("companies");
+
+
+
+    app.get('/api/jobs', async (req, res) => {
+      const query = {};
+      if(req.query.companyId){
+        query.companyId = req.query.companyId;
+      console.log(req.query.companyId);
+      }
+      if(req.query.status){
+        query.status = req.query.status;
+      }
+      const cursor = jobCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+
+    app.post('/api/jobs', async (req, res) => {
+      const job = req.body;
+      const result = await jobCollection.insertOne(job);
+      res.send(result);
+  });
+
+  app.get('/api/my/companies', async (req, res) => {
+    const query = {};
+    if(req.query.recruiterId){
+      query.recruiterId = req.query.recruiterId;
+    }
+    const result = await companyCollection.findOne(query);
+    res.send(result);
+
+
+  })
+
+  // company related api
+  app.post('/api/companies', async (req, res) => {
+    const company = req.body;
+    const result = await companyCollection.insertOne(company);
+    res.send(result);
+});
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
